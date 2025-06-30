f9072c2c29bfa856fe1e0aaf1b85d1a4f60d4d27
kimyena1123 <kyq104@gmail.com>
2025-06-30 14:51:05 +0900

    study: REST API 테스트(핑퐁) 초기 프로젝트 준비

---
+++ preview_restapi/src/main/java/com/study/preview_restapi/PreviewRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.study.preview_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreviewRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreviewRestapiApplication.class, args);
+    }
+
+}
---
+++ preview_restapi/src/test/groovy/com/study/preview_restapi/controller/PingControllerSpec.groovy
@@ -0,0 +1,17 @@
+package com.study.preview_restapi.controller
+
+import spock.lang.Specification
+
+class PingControllerSpec extends Specification {
+
+    def "Ping API는 받은 count 값에 1을 더한 값을 리턴한다"() {
+        given:
+        false
+
+        when:
+        false
+
+        then:
+        false
+    }
+}
