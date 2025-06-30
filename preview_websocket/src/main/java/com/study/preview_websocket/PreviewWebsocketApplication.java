b515b3a508a60ec9c2367631daf1816744ff9007
kimyena1123 <kyq104@gmail.com>
2025-06-30 17:00:10 +0900

    study: WebSocket 테스트 초기 프로젝트 준비

---
+++ preview_websocket/src/main/java/com/study/preview_websocket/PreviewWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.study.preview_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreviewWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreviewWebsocketApplication.class, args);
+    }
+
+}
---
+++ preview_websocket/src/test/java/com/study/preview_websocket/PreviewWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.preview_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreviewWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
