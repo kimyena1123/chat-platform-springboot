4d64f43150b9a4a5256ec2a6f2f9e57d01bfd18e
kimyena1123 <kyq104@gmail.com>
2025-07-11 23:50:11 +0900

    study: Group Chat - 프로젝트 기본 세팅

---
+++ practice/preview_group_chat/backend/src/main/java/com/messagesystem/backend/dto/Message.java
@@ -0,0 +1,4 @@
+package com.messagesystem.backend.dto;
+
+public record Message(String username, String content) {
+}
---
+++ practice/preview_group_chat/backend/src/test/groovy/com/messagesystem/backend/BackendApplicationSpec.groovy
@@ -0,0 +1,15 @@
+package com.messagesystem.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification;
+
+@SpringBootTest(classes = BackendApplication)
+class BackendApplicationSpec extends Specification{
+
+    void contextLoads() {
+        expect:
+        true
+    }
+
+}
